9650b91dced7008f76dc9475d842d40f45070085
Caio Matheus dos Santos Abra <caioabra41@gmail.com>
2023-02-19 01:59:04 -0300

    sistema de gridagem

--- app/views/home.php
+++ app/views/home.php
@@ -1,2 +1,14 @@
-<h1>Bem-vindo à minha página inicial!</h1>
-<p>Esta é uma página de exemplo criada com PHP.</p>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1>Bem-vindo à minha página inicial!</h1>
+            <p>Esta é uma página de exemplo criada com PHP.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4 col-md-6">col-4</div>
+        <div class="col-4 col-md-6">col-4</div>
+        <div class="col-4 col-md-12">col-4</div>
+    </div>
+</div>
